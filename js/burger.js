0bec4332f39ec7e5d1e820d3909145efeb082272
viko4ka <viku81@ya.ru>
2025-01-22 11:29:37 +0300

    click

--- js/burger.js
+++ js/burger.js
@@ -1,8 +1,15 @@
 const burgerToggle = document.querySelector('.burger-toggle');
-const menuItems = document.querySelector('.menu-items');
 const burgerMenu = document.querySelector('.burger-menu');
+const menuItems = document.querySelectorAll('.menu-items a');
+
 
 burgerToggle.addEventListener('click', () => {
     burgerToggle.classList.toggle('open');
     burgerMenu.classList.toggle('open');
+});
+menuItems.forEach(item => {
+    item.addEventListener('click', () => {
+        burgerMenu.classList.remove('open');  
+        burgerToggle.classList.remove('open'); 
+    });
 });
